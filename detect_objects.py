fb66a287ab4ad7e5a3f8a1df50cf1012c68427ee
samsung-chungso <digital_trans@naver.com>
2024-03-17 11:17:20 +0900

    NMS option

--- detect_objects.py
+++ detect_objects.py
@@ -1,9 +1,9 @@
 import arcpy
 arcpy.ImportToolbox(r"@\Image Analyst Tools.tbx")
-with arcpy.EnvManager(extent='126.939047297046 37.5589669544028 126.953161904103 37.5688393955522 GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]', cellSize=1, processorType="CPU", scratchWorkspace=r""):
+with arcpy.EnvManager(extent='126.938797778772 37.5590594680161 126.952584966152 37.5687028959543 GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]', cellSize=1, processorType="CPU", scratchWorkspace=r""):
     arcpy.ia.DetectObjectsUsingDeepLearning(
         in_raster="영상",
-        out_detected_objects=r"C:\Users\Geocontents\Documents\ArcGIS\Projects\Road Extraction\Default.gdb\ewha_road_revise",
+        out_detected_objects=r"C:\Users\Geocontents\Documents\ArcGIS\Projects\Road Extraction\Default.gdb\ewha_road_NMS",
         in_model_definition=r"C:\Users\Geocontents\Downloads\RoadExtraction_Global.dlpk",
         arguments="batch_size 4",
         run_nms="NMS",
