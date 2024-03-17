417e98b5d9d5e03f471b1306918564b76321a956
samsung-chungso <digital_trans@naver.com>
2024-03-17 11:18:00 +0900

    no NMS

--- detect_objects.py
+++ detect_objects.py
@@ -1,12 +1,12 @@
 import arcpy
 arcpy.ImportToolbox(r"@\Image Analyst Tools.tbx")
-with arcpy.EnvManager(extent='126.938797778772 37.5590594680161 126.952584966152 37.5687028959543 GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]', cellSize=1, processorType="CPU", scratchWorkspace=r""):
+with arcpy.EnvManager(extent='126.940649473687 37.5590302585642 126.951820801173 37.5694486166727 GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]', cellSize=1, processorType="CPU", scratchWorkspace=r""):
     arcpy.ia.DetectObjectsUsingDeepLearning(
         in_raster="영상",
-        out_detected_objects=r"C:\Users\Geocontents\Documents\ArcGIS\Projects\Road Extraction\Default.gdb\ewha_road_NMS",
+        out_detected_objects=r"C:\Users\Geocontents\Documents\ArcGIS\Projects\Road Extraction\Default.gdb\ewha_road_less_extent",
         in_model_definition=r"C:\Users\Geocontents\Downloads\RoadExtraction_Global.dlpk",
         arguments="batch_size 4",
-        run_nms="NMS",
+        run_nms="NO_NMS",
         confidence_score_field="Confidence",
         class_value_field="Class",
         max_overlap_ratio=0,
